77ba8106417ed114e2c8a94a2d3caa1eb1fa664d
raingerber <rain_gerber@yahoo.com>
2014-02-19 19:05:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,15 +10,21 @@ var connection = mysql.createConnection({
     database: "cs4720mra9cg"
 });
  
-http.createServer(function (request, response) { 
+http.createServer(function (request, response) {
+	// get the parameter from the end of the url
 	var req = request.url.split("/");
+	// replace "%20" with a blank space for parameters that have multiple words
 	var str = req[1].replace("%20"," ");
+	// default select statement
 	var inp = "SELECT * FROM food";
+	// if there is a valid name parameter, update the select statement
 	if (str!="favicon.ico" && str!="") {
 		inp = inp.concat(" WHERE name='", str,"'");
 	}
+	// access the database
 	connection.query(inp, function (error, rows, fields) {
         response.writeHead(200, {'Content-Type': 'text/html'});
+        	// have a link to return to the index page
 		response.write("<a href='http://plato.cs.virginia.edu/~mra9cg/hw4/'>Return</a><br><br>");
         response.write(JSON.stringify(rows));
 		response.end();
